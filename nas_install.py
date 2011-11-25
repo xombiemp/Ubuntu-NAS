36bf7eb5ee8672d2ec00b445f67498e35d1bb1ff
Andrew Parker <andrewmichaelparker@gmail.com>
2011-11-24 23:49:57 -0700

    Fixed the update script to use the git repository for libtorrent and rtorrent.

--- nas_install.py
+++ nas_install.py
@@ -356,7 +356,7 @@ fi
 
 
 echo "INSTALLING PREREQS"
-sudo apt-get install git-core subversion build-essential automake libtool libcppunit-dev libcurl4-openssl-dev libsigc++-2.0-dev unzip unrar curl libncurses-dev -y
+sudo apt-get install git-core subversion build-essential automake libtool libcppunit-dev libcurl4-openssl-dev libsigc++-2.0-dev unzip unrar curl libncurses-dev ffmpeg -y
 sudo apt-get install apache2 php5 php5-cli php5-curl php5-geoip -y
 
 sudo apt-get install libapache2-mod-scgi -y
@@ -584,8 +584,8 @@ rutorrent_path="/var/www/rutorrent"
 plugins_path="/var/www/rutorrent/plugins"
 
 xmlrpc_c_svn="http://xmlrpc-c.svn.sourceforge.net/svnroot/xmlrpc-c/advanced"
-libtorrent_svn="svn://rakshasa.no/libtorrent/trunk/libtorrent"
-rtorrent_svn="svn://rakshasa.no/libtorrent/trunk/rtorrent"
+libtorrent_git="git://github.com/rakshasa/libtorrent.git"
+rtorrent_git="git://github.com/rakshasa/rtorrent.git"
 rutorrent_svn="http://rutorrent.googlecode.com/svn/trunk/rutorrent"
 plugins_svn="http://rutorrent.googlecode.com/svn/trunk/plugins"
 
@@ -611,46 +611,46 @@ fi
 
 echo -e "UPDATING libtorrent"
 cd $libtorrent_path
-oldsvn=$(svn info | grep "Revision" | cut -c11-)
-newsvn=$(svn info $libtorrent_svn | grep "Revision" | cut -c11-)
-if [ "$newsvn" != "$oldsvn" ] ; then
-  service rtorrentd stop
-  svn up
+oldgit=$(git describe --always)
+service rtorrentd stop
+git pull
+newgit=$(git describe --always)
+if [ "$newgit" != "$oldgit" ] ; then
   ./autogen.sh
   ./configure
   make
   make install
-  echo -e "libtorrent updated from $oldsvn to $newsvn\\n"
+  echo -e "libtorrent updated from $oldgit to $newgit\\n"
   sleep 2
   updated="yes"
 else
-  echo -e "libtorrent is already at the latest version: $newsvn\\n"
+  echo -e "libtorrent is already at the latest version: $newgit\\n"
   sleep 2
 fi
 
 
 echo -e "UPDATING rtorrent"
 cd $rtorrent_path
-oldsvn=$(svn info | grep "Revision" | cut -c11-)
-newsvn=$(svn info $rtorrent_svn | grep "Revision" | cut -c11-)
-if [ "$newsvn" != "$oldsvn" ] || [ "$updated" == "yes" ] ; then
-  service rtorrentd stop
-  svn up
+oldgit=$(git describe --always)
+service rtorrentd stop
+git pull
+newgit=$(git describe --always)
+if [ "$newgit" != "$oldgit" ] || [ "$updated" == "yes" ] ; then
   ./autogen.sh
   ./configure --with-xmlrpc-c
   make
   make install
   ldconfig
-  if [ "$newsvn" != "$oldsvn" ] ; then
-    echo -e "rtorrent updated from $oldsvn to $newsvn\\n"
+  if [ "$newgit" != "$oldgit" ] ; then
+    echo -e "rtorrent updated from $oldgit to $newgit\\n"
     sleep 2
   else
-    echo -e "rtorrent was recompiled and is at version $newsvn\\n"
+    echo -e "rtorrent was recompiled and is at version $newgit\\n"
     sleep 2
   fi
   updated="yes"
 else
-  echo -e "rtorrent is already at the latest version: $newsvn\\n"
+  echo -e "rtorrent is already at the latest version: $newgit\\n"
   sleep 2
 fi
 
@@ -694,6 +694,7 @@ if [ "$updated" == "yes" ] ; then
   service rtorrentd start
 else
   echo -e "Already running the latest version of everything!\\n"
+  service rtorrentd start
 fi
 """
   f = open('/usr/local/bin/rtorrent_update', 'w')
